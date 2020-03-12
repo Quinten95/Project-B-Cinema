12ec9835fd9801cf3b607b3c9a95d0ef4ad6c54d
EHaze1999 <58138330+EHaze1999@users.noreply.github.com>
2020-03-12 12:10:17 +0100

    test

--- Project B/Program.cs
+++ Project B/Program.cs
@@ -7,6 +7,7 @@ namespace Project_B
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Test Line");
         }
     }
 }
