115b546477a6c57e8e4ad1b8ae77f07ec8042568
Quinten95 <quinten103@gmail.com>
2020-03-23 08:32:16 +0100

    welkomst banner gemaakt

--- Project B/Program.cs
+++ Project B/Program.cs
@@ -6,7 +6,26 @@ namespace Project_B
     {
         static void Main(string[] args)
         {
+            displayWelcomeMsg();
+        }
 
+        static void displayWelcomeMsg()
+        {
+            Console.WriteLine("------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("|    ========   ===   ====     ===   ===========   ====        ====         =====         ===     === |");
+            Console.WriteLine("|  ======       ===   =====    ===   ===========   =====      =====        === ===         ===   ===  |");
+            Console.WriteLine("| ====          ===   === ==   ===   ===           === ==    == ===       ===   ===         === ===   |");
+            Console.WriteLine("| ===           ===   ===  ==  ===   ==========    ===  ==  ==  ===      ===========         =====    |");
+            Console.WriteLine("| ====          ===   ===   == ===   ===           ===   ====   ===     ====     ====       === ===   |");
+            Console.WriteLine("|  ======       ===   ===    =====   ===========   ===    ==    ===    ===         ===     ===   ===  |");
+            Console.WriteLine("|    ========   ===   ===     ====   ===========   ===          ===   ===           ===   ===     === |");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("                              -----------------------------------------");
+            Console.WriteLine("                             |      Welkom op de site van CinemaX!     |");
+            Console.WriteLine("                             | Hier kunt u zien welke films er draaien.|");
+            Console.WriteLine("                             |      Ook kunt u tickets bestellen!      |");
+            Console.WriteLine("                              -----------------------------------------");
         }
     }
 }
