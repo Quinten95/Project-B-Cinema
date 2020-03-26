17ab3022bfca50b108b432f347864c2b8f5d1187
Quinten95 <quinten103@gmail.com>
2020-03-26 13:53:41 +0100

    de gebruiker moet zijn gegevens nu invullen bij het reserveren van een ticket

--- Project B/Program.cs
+++ Project B/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Project_B
 {
@@ -86,33 +87,70 @@ namespace Project_B
             Console.WriteLine("Type \'y\' om uw keuze te bevestigen en \'n\' om uw keuze te wijzigen: ");
             
             string userConfirmation = "";
-            while (userConfirmation != "y" || userConfirmation != "Y")
+            while (userConfirmation != "y" && userConfirmation != "Y")
             {
                 userConfirmation = Console.ReadLine();
-                if (userConfirmation == "y" || userConfirmation == "Y")
+                switch (userConfirmation)
                 {
-                    Ticket ticket = new Ticket(movie);
-                    break;
-                }
-                else if (userConfirmation == "n" || userConfirmation == "N")
-                {
-                    Console.WriteLine("Voor welke film wilt u tickets kopen:");
-                    int userChoice = -1;
-                    while (userChoice < 1 || userChoice > Movies.movieList.Count)
-                    {
-                        try
+                    case "y":
+                    case "Y":
                         {
-                            userChoice = int.Parse(Console.ReadLine());
-                            Movies movieToReserve = (Movies)Movies.movieList[userChoice - 1];
-                            reserveTicket(movieToReserve);
+                            Ticket ticket = new Ticket(movie);
+
+                            Console.WriteLine("Voert u alstublieft uw naam in:");
+                            string customerName = Console.ReadLine();
+
+                            Console.WriteLine("Voert u alstublieft uw e-mail adres in:");
+                            string customerEmail = Console.ReadLine();
+
+                            Console.WriteLine("Voert u alsublieft uw geboortedatum in (dd/mm/yyyy):");
+                            DateTime? customerBirthDay = null;
+                            while (customerBirthDay == null)
+                            {
+                                try
+                                {
+                                    string customerBDayStr = Console.ReadLine();
+                                    CultureInfo dutchCI = new CultureInfo("nl-NL", false);
+                                    customerBirthDay = DateTime.Parse(customerBDayStr, dutchCI);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("Uw geboortedatum moet als dd/mm/yyyy ingevoerd worden, bijv: 01/01/2020:");
+                                }
+                            }
+                            Customer customer = new Customer(customerName, customerBirthDay, customerEmail);
+                            break;
                         }
-                        catch (Exception e)
+
+                    case "n":
+                    case "N":
                         {
-                            Console.WriteLine("Voert u a.u.b. een film nummer in:");
+                            Console.WriteLine("Voor welke film wilt u tickets kopen:");
+                            int userChoice = -1;
+                            while (userChoice < 1 || userChoice > Movies.movieList.Count)
+                            {
+                                try
+                                {
+                                    userChoice = int.Parse(Console.ReadLine());
+                                    Movies movieToReserve = (Movies)Movies.movieList[userChoice - 1];
+                                    reserveTicket(movieToReserve);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("Voert u a.u.b. een film nummer in:");
+                                }
+                            }
+
+                            break;
                         }
-                    }
+
+                    default:
+                        Console.WriteLine("Kiest u a.u.b. voor één van de opties");
+                        break;
                 }
             }
+
+            
         }
     }
 }
