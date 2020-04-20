fc40e8683138b4f1831302f2d54b5477755071c7
EHaze1999 <58138330+EHaze1999@users.noreply.github.com>
2020-04-20 09:26:45 +0200

    Basis ticket prijs koppelen

--- Project B/Customer.cs
+++ Project B/Customer.cs
@@ -10,11 +10,18 @@ namespace Project_B
         private DateTime birthday;
         public int Age { get; set; }
         public string Email { get; set; }
+        public double priceOfTicket;
+
+        public double calcTicketPrice()
+        {
+            return 2.0;
+        }
 
         public Customer(string customerName, DateTime birthday)
         {
             this.customerName = customerName;
             this.birthday = birthday;
+            this.priceOfTicket = calcTicketPrice();
         }
     }
 }
